70a059758c25aa99eb3497015e4b6df3b393f8eb
Yevinn <kevin@yevinn.com>
2020-03-11 10:33:21 -0600

    Add Message User JS and Bell Notifications

--- app.js
+++ app.js
@@ -108,3 +108,19 @@ let mobileChart = new Chart(mobileCanvas, {
   data: mobileData,
   options: mobileOptions
 });
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', (event) => {
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.value === "") {
+    alert("Please fill our user field before sending");
+  } else if (message.value === "") {
+    alert("Please fill out message field before sending");
+  } else {
+    alert(`Message successfully sent to: ${user.value}`);
+  }
+});
